d52ffdc3ea7c329e0ee13a2d9b834826c48870ab
AlexRodriguez13 <bryanalexandercb13@gmail.com>
2022-04-06 09:19:17 -0600

    Class DetalleActivoEmpleado

---
+++ Domain/Entities/DetalleActivoEmpleado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class DetalleActivoEmpleado
+    {
+        public int Id { get; set; }
+        public int Activo { get; set; }
+
+        public DateTime Fecha { get; set; }
+        public bool Estado { get; set; }
+
+
+
+
+    }
+}
