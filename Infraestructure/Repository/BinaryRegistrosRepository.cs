6ebc3e6702a71fa2e3edd8c7a23708ebafedd7e9
AlexRodriguez13 <bryanalexandercb13@gmail.com>
2022-04-07 09:38:41 -0600

    BinaryRegistroRepository

--- Infraestructure/Repository/BinaryRegistrosRepository.cs
+++ Infraestructure/Repository/BinaryRegistrosRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Infraestructure.Repository
 {
-   public class BinaryRegistrosRepository
+   public class BinaryRegistrosRepository : IRegistroModel
     {
         RAFContext Context;
         public BinaryRegistrosRepository()
